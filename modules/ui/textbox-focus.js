4ef8cf66c146db3b566ff0b4c6ef63d0ef59b479
Finwickle <14930335+Finwickle@users.noreply.github.com>
2025-03-26 18:27:12 +0100

    Delete modules/ui/textbox-focus.js
    Removed fixes for textboxes and missing icons/text, that were breaking the multiplayer lobby (and unknown if these fixes are still needed).

--- modules/ui/textbox-focus.js
+++
@@ -1,6 +0,0 @@
-import '/core/ui/components/fxs-textbox.js';
-
-const definition = Controls.getDefinition('fxs-textbox');
-definition.styles ??= [];
-definition.styles.push('fs://game/bobobunicorn-ui-tweaks/modules/ui/textbox-focus.css')
-
